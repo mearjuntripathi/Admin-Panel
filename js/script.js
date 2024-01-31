b50daa3fb2e3b809f17cf2d86168c2c39929be08
mearjuntripathi <mearjuntripathi@gmail.com>
2024-01-31 21:46:29 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
+const body = document.querySelector('body');
+const modeToggle = body.querySelector('.mode-toggle');
+
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
+    body.classList.toggle('dark');
+}
+
+modeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    if (body.classList.contains('dark')) {
+        localStorage.setItem("mode", "dark");
+    } else {
+        localStorage.setItem("mode", "light");
+    }
+});
+
 function initializeEventListeners() {
-    const body = document.querySelector('body');
-    const modeToggle = body.querySelector('.mode-toggle');
     const sidebarToggle = body.querySelector('.sidebar-toggle');
     const sidebar = body.querySelector('nav');
-
-    let getMode = localStorage.getItem("mode");
-    if (getMode && getMode === "dark") {
-        body.classList.toggle('dark');
-    }
 
     let getStatus = localStorage.getItem('status');
     if (getStatus && getStatus === "close") {
         sidebar.classList.toggle("close");
     }
-
-    modeToggle.addEventListener('click', () => {
-        body.classList.toggle('dark');
-        if (body.classList.contains('dark')) {
-            localStorage.setItem("mode", "dark");
-        } else {
-            localStorage.setItem("mode", "light");
-        }
-    });
 
     sidebarToggle.addEventListener('click', () => {
         sidebar.classList.toggle("close");
@@ -33,6 +34,8 @@ function initializeEventListeners() {
     });
 }
 
+
+initializeEventListeners();
 
 var panel = document.getElementById('panel');
 
